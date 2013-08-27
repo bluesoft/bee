8ff0a9a4874b5c1434cf0f2090bcb379082bca52
Luiz Faias Junior <junior@bluesoft.com.br>
2013-08-27 15:24:09 -0300

    Fixing a date issue when inserting into Oracle

--- src/main/groovy/br/com/bluesoft/bee/BeeOracleSchemaCreator.groovy
+++ src/main/groovy/br/com/bluesoft/bee/BeeOracleSchemaCreator.groovy
@@ -5,5 +5,9 @@ import br.com.bluesoft.bee.service.BeeWriter;
 import br.com.bluesoft.bee.importer.JsonImporter
 
 class BeeOracleSchemaCreator extends BeeSchemaCreator {
-	
+
+	void createCoreData(def file, def schema, def dataFolderPath) {
+		file.append("alter session set nls_date_format = 'yyyy-mm-dd';\n\n", 'utf-8')
+		super.createCoreData(file, schema, dataFolderPath)
+	}
 }
