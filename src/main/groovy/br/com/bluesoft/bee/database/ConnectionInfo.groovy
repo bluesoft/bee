d95bc6e990448181e9bffb2592c1d192503e6ef0
Daniel Faria Gomes <danielfariati@gmail.com>
2014-10-22 11:31:29 -0200

    Removendo código não utilizado

--- src/main/groovy/br/com/bluesoft/bee/database/ConnectionInfo.groovy
+++ src/main/groovy/br/com/bluesoft/bee/database/ConnectionInfo.groovy
@@ -4,17 +4,6 @@ import br.com.bluesoft.bee.util.PropertiesUtil
 import groovy.sql.Sql
 
 class ConnectionInfo {
-
-	String host
-	String username
-	String password
-	String port = 1521
-	String serviceName = "serverdb"
-
-	Sql getSqlInstanceForOracle() {
-		Sql.newInstance("jdbc:oracle:thin:@${host}:${port}:${serviceName}", username, password, "oracle.jdbc.driver.OracleDriver")
-	}
-
 	static def createDatabaseConnection(File configFile, def key) {
 		def config = PropertiesUtil.readDatabaseConfig(configFile, key)
 		if (config != null) {
